e3d0abfb7154af2e4929984760dcec7619790778
xzmeasy <1280428201@qq.com>
2020-01-19 10:16:58 +0800

    create video-pojo

---
+++ video-pojo/src/main/java/com/video/pojo/VideoPojoApplication.java
@@ -0,0 +1,13 @@
+package com.video.pojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoPojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoPojoApplication.class, args);
+    }
+
+}
---
+++ video-pojo/src/test/java/com/video/pojo/VideoPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.pojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
