307d3c8cd611c66aa67021b8b67497ce4d3ddca2
xzmeasy <1280428201@qq.com>
2020-01-19 10:22:08 +0800

    create video-mapper

---
+++ video-mapper/src/main/java/com/video/mapper/VideoMapperApplication.java
@@ -0,0 +1,13 @@
+package com.video.mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoMapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoMapperApplication.class, args);
+    }
+
+}
---
+++ video-mapper/src/test/java/com/video/mapper/VideoMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoMapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
