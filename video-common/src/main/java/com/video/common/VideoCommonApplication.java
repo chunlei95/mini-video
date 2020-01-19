27f043f0da62f24bdbbffc2e8a96f9bb51b14755
xzmeasy <1280428201@qq.com>
2020-01-19 10:12:13 +0800

    create video-common

---
+++ video-common/src/main/java/com/video/common/VideoCommonApplication.java
@@ -0,0 +1,13 @@
+package com.video.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoCommonApplication.class, args);
+    }
+
+}
---
+++ video-common/src/test/java/com/video/common/VideoCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
