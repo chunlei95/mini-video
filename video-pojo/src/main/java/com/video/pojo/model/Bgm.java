6c1773f430b493db0140404aaee81e8cf9acd596
xzmeasy <1280428201@qq.com>
2020-02-14 10:13:41 +0800

    bgm中添加createTime字段,并为其添加swagger注释

--- video-pojo/src/main/java/com/video/pojo/model/Bgm.java
+++ video-pojo/src/main/java/com/video/pojo/model/Bgm.java
@@ -1,29 +1,32 @@
 package com.video.pojo.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.util.Date;
 
 /**
  * @author xzmeasy
  * @date 2020/1/20
  */
 @Data
+@ApiModel("背景音乐实体类")
 public class Bgm {
 
+    @ApiModelProperty("唯一标识符")
     private String id;
 
-    /**
-     * The author of background music.
-     */
+    @ApiModelProperty("背景音乐的作者")
     private String author;
 
-    /**
-     * The name of background music.
-     */
+    @ApiModelProperty("背景音乐的名称")
     private String name;
 
-    /**
-     * The access path of background music.
-     */
+    @ApiModelProperty("背景音乐的保存路径")
     private String path;
+
+    @ApiModelProperty("背景音乐添加时间")
+    private Date createTime = new Date();
 
 }
